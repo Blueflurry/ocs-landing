02616412d92feded98387c053eada03dae9f7249
siddhantj929 <siddhantj929@gmail.com>
2024-11-11 18:34:58 +0530

    added google tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,6 +29,23 @@ const SpaceGrotesk = localFont({
     variable: "--font-space-grotesk",
 });
 
+const googleTag = () => {
+    return (
+        <script>
+            {(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+                var f = d.getElementsByTagName(s)[0],
+                    j = d.createElement(s),
+                    dl = l != "dataLayer" ? "&l=" + l : "";
+                j.async = true;
+                j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-P9ZDRGN9")}
+        </script>
+    );
+};
+
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function RootLayout({ children }) {
@@ -59,8 +76,17 @@ export default function RootLayout({ children }) {
             <head>
                 <title>OneClick Stays - Perfect luxury stays in budget</title>
                 <link rel="shortcut icon" href="favicon.png" type="image/png" />
+                <googleTag />
             </head>
             <body className={`${DMSans.variable} ${ClashDisplay.variable} ${Raleway.variable} ${SpaceGrotesk.variable} ${inter.variable}`}>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-P9ZDRGN9"
+                        height="0"
+                        width="0"
+                        style="display:none;visibility:hidden"
+                    ></iframe>
+                </noscript>
                 {children}
             </body>
         </html>
